typeling1578/vertical-tabs

commit 37a13e7c7d5bb5cf6b2b898d5f74049b3946f8e5

Simplify scrolling logic

src/sidebar/tab.js
@@ -154,19 +154,8 @@ SideTab.prototype = {
     }
     // Avoid an expensive sync reflow (scrolling).
     requestAnimationFrame(() => {
-      this._scrollIntoView();
+      this.view.scrollIntoView({block: "nearest", behavior: "smooth"});
     });
-  },
-  _scrollIntoView() {
-    const {top: parentTop, height} = this.view.parentNode.parentNode.getBoundingClientRect();
-    let {top, bottom} = this.view.getBoundingClientRect();
-    top -= parentTop;
-    bottom -= parentTop;
-    if (top < 0) {
-      this.view.scrollIntoView({block: "start", behavior: "smooth"});
-    } else if (bottom > height) {
-      this.view.scrollIntoView({block: "end", behavior: "smooth"});
-    }
   },
   updateVisibility(show) {
     this.visible = show;

commit e4ee2d8433979f585d7d2187d8781ed7dee5a4ed

Do not scroll when tab is already in view
Regression introduced in:
37a13e7c7d5bb5cf6b2b898d5f74049b3946f8e5

src/sidebar/tab.js
@@ -154,11 +154,18 @@ SideTab.prototype = {
     }
     // Avoid an expensive sync reflow (scrolling).
     requestAnimationFrame(() => {
-      this.view.scrollIntoView();
+      this._scrollIntoViewIfNeeded();
       // workaround for https://bugzilla.mozilla.org/show_bug.cgi?id=1139745#c7
       // we still make a first scrollIntoView so that it starts scrolling right away
-      setTimeout(() => this.view.scrollIntoView(), 100);
+      setTimeout(() => this._scrollIntoViewIfNeeded(), 100);
     });
+  },
+  _scrollIntoViewIfNeeded() {
+    const {top: parentTop, height} = this.view.parentNode.getBoundingClientRect();
+    let {top, bottom} = this.view.getBoundingClientRect();
+    if ((top - parentTop) < 0 || (bottom - parentTop) > height) {
+      this.view.scrollIntoView({block: "nearest"});
+    }
   },
   updateVisibility(show) {
     this.visible = show;

src/sidebar/tablist.js
@@ -208,10 +208,6 @@ TabList.prototype = {
     }
   },
   _onMouseOver(e) {
-    if (!SideTab.isTabEvent(e)) {
-      return;
-    }
-
     const tabId = SideTab.tabIdForEvent(e);
     if (tabId === this._active) {
       this.getTabById(tabId).scrollIntoView();