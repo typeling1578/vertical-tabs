6338d9a655370c8c273285e2e1e2de00098a17f6
ariasuni <perso@hack-libre.org>
2019-04-03 15:46:49 +0200

    Make toolbar button indicator always work correctly
    * Manage state by window
    * Add badge text for new window with sidebar closed

--- src/background/background.js
+++ src/background/background.js
@@ -1,19 +1,24 @@
+const BADGE_DISABLED_BACKGROUND = "#38383d";
+const BADGE_ENABLED_BACKGROUND = "#058b00";
+
 class TabCenterBackground {
   constructor() {
     this.openedSidebarWindows = {};
     browser.runtime.onConnect.addListener(port => this.onConnect(port));
     browser.browserAction.onClicked.addListener((tab) => this.onClick(tab));
+
+    browser.browserAction.setBadgeText({text: "⏻"});
     browser.browserAction.setBadgeTextColor({color: "white"});
+    browser.browserAction.setBadgeBackgroundColor({color: BADGE_DISABLED_BACKGROUND});
   }
 
   onConnect(port) {
     const windowId = parseInt(port.name);
     this.openedSidebarWindows[windowId] = port;
-    browser.browserAction.setBadgeText({text: "⏻", windowId});
-    browser.browserAction.setBadgeBackgroundColor({color: "#058b00"});
+    browser.browserAction.setBadgeBackgroundColor({color: BADGE_ENABLED_BACKGROUND, windowId});
     port.onDisconnect.addListener(port => {
       delete this.openedSidebarWindows[parseInt(port.name)];
-      browser.browserAction.setBadgeBackgroundColor({color: "#38383d"});
+      browser.browserAction.setBadgeBackgroundColor({color: BADGE_DISABLED_BACKGROUND, windowId});
     });
   }
 
