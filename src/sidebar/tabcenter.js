f3d23e28bf5725d3f0779475d51d26e9a8a31042
ariasuni <perso@hack-libre.org>
2019-08-02 00:18:18 +0200

    Theme integration: invert background and active color the are light
    Tab list looks better when light themes have a lighter background

--- src/sidebar/tabcenter.js
+++ src/sidebar/tabcenter.js
@@ -212,6 +212,14 @@ export default class TabCenter {
       document.body.hasOwnProperty("--input-selected-text") &&
         document.body.hasOwnProperty("--input-selected-text-background"),
     );
+
+    const style = document.body.style;
+    const background = style.getPropertyValue("--background");
+    const tabActiveBackground = style.getPropertyValue("--tab-active-background");
+    if (background !== "" && tabActiveBackground !== "" && isLight(background)) {
+      style.setProperty("--background", tabActiveBackground);
+      style.setProperty("--tab-active-background", background);
+    }
   }
 
   _resetTheme() {
@@ -246,4 +254,23 @@ function setBrowserActionColor(color) {
       32: svgStr,
     },
   });
+}
+
+// from https://awik.io/determine-color-bright-dark-using-javascript/
+function isLight(color) {
+  let r, g, b;
+  if (color.match(/^rgb/)) {
+    // If HEX, store the red, green, blue values in separate variables
+    [_, r, g, b] = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+  } else {
+    // If RGB, convert it to HEX: http://gist.github.com/983661
+    color = `0x${color.slice(1).replace(color.length < 5 && /./g, "$&$&")}`;
+    r = color >> 16;
+    g = (color >> 8) & 255;
+    b = color & 255;
+  }
+
+  // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+  const hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
+  return hsp > 127.5;
 }
