818494ca54f980e188458e878b1db9d3376f16ee
ariasuni <perso@hack-libre.org>
2019-08-12 15:52:24 +0200

    Add comments and change name of a function to make it more accurate

--- src/sidebar/tabcenter.js
+++ src/sidebar/tabcenter.js
@@ -138,6 +138,8 @@ export default class TabCenter {
 
   _applyTheme(theme) {
     const style = document.body.style;
+
+    // fallbacks for theme colors
     const cssToThemeProp = {
       "--background": ["frame", "accentcolor"],
       "--button-background-active": ["button_background_active"],
@@ -168,6 +170,7 @@ export default class TabCenter {
       "sidebar-text": ["sidebar_text"],
     };
 
+    // get the effective values we will be using
     const themeColors = {};
     for (const [cssVar, themeProps] of Object.entries(cssToThemeProp)) {
       for (const prop of themeProps) {
@@ -182,12 +185,13 @@ export default class TabCenter {
       }
     }
 
-    setBrowserActionColor(
+    setButtonsActionColor(
       themeColors["--icons"] || "#5a5b5c",
       themeColors["sidebar-text"] || "#5a5b5c",
     );
     delete themeColors["sidebar-text"];
 
+    // swap background and tab-active-background colors if background is light
     if (themeColors["--background"] !== null && isLight(themeColors["--background"])) {
       if (themeColors["--tab-active-background"] !== null) {
         let tmp = themeColors["--background"];
@@ -200,6 +204,7 @@ export default class TabCenter {
       }
     }
 
+    // apply color if one was found, otherwise remove var and use default style
     for (const [cssVar, color] of Object.entries(themeColors)) {
       if (color !== null) {
         style.setProperty(cssVar, color);
@@ -214,7 +219,6 @@ export default class TabCenter {
         themeColors["--input-selected-text-background"] !== null,
     );
   }
-
   startTests() {
     const script = document.createElement("script");
     script.src = "../test/index.js";
@@ -222,7 +226,8 @@ export default class TabCenter {
   }
 }
 
-function setBrowserActionColor(browserColor, sidebarColor) {
+// Toolbar icon takes a different color than the sidebar header icon
+function setButtonsActionColor(browserColor, sidebarColor) {
   // src/tabcenter.svg but a little reduced
   const svgStr =
     "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3Csvg xmlns='http://www.w3.org/2000/svg' height='128' width='128' viewBox='0 0 16 16'%3E%3Cdefs%3E%3Csymbol id='shape'%3E%3Cpath d='M3,1h10a3,3,0,0,1,3,3v8a3,3,0,0,1,-3,3h-10a3,3,0,0,1,-3,-3v-8a3,3,0,0,1,3,-3Z M3,3h 4a1,1,0,0,1,1,1v8a1,1,0,0,1,-1,1h -4a1,1,0,0,1,-1,-1v-8a1,1,0,0,1,1,-1Z' fill-rule='evenodd' /%3E%3Ccircle cx='3.5' cy='4.5' r='.6' /%3E%3Ccircle cx='3.5' cy='6.5' r='.6' /%3E%3Ccircle cx='3.5' cy='8.5' r='.6' /%3E%3Crect x='4.75' y='4' height='1' width='2.25' rx='.5' ry='.5' /%3E%3Crect x='4.75' y='6' height='1' width='2.25' rx='.5' ry='.5' /%3E%3Crect x='4.75' y='8' height='1' width='2.25' rx='.5' ry='.5' /%3E%3C/symbol%3E%3C/defs%3E%3Cuse class='theme' id='default' fill='$fillcolor' href='%23shape' /%3E%3C/svg%3E%0A";
