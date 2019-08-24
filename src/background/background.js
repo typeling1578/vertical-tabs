8d725ab214c295fca5b2c4d201e29859e251a4e5
ariasuni <perso@hack-libre.org>
2019-08-24 17:11:12 +0200

    Fix theme handling for icons in sidebar header and browser toolbar

--- src/background/background.js
+++ src/background/background.js
@@ -14,19 +14,13 @@ class TabCenterBackground {
     if (!browser.theme.onUpdated) {
       return;
     }
-    const windows = await browser.windows.getAll();
-    for (const window of windows) {
-      browser.theme.getCurrent(window.id).then(theme => {
-        this.onThemeUpdated(theme, window.id);
-      });
-    }
-    browser.theme.onUpdated.addListener(({ theme, windowId }) =>
-      this.onThemeUpdated(theme, windowId),
-    );
+    const theme = await browser.theme.getCurrent();
+    this.onThemeUpdated(theme);
+    browser.theme.onUpdated.addListener(({ theme }) => this.onThemeUpdated(theme));
   }
 
-  onThemeUpdated(theme, windowId) {
-    const iconsColorFallback = [
+  onThemeUpdated(theme) {
+    const browserColorFallback = [
       "icons",
       "toolbar_text",
       "bookmark_text",
@@ -35,13 +29,13 @@ class TabCenterBackground {
     ];
 
     // get the effective values we will be using
-    let iconsColor = "#5a5b5c";
-    for (const prop of iconsColorFallback) {
+    let browserColor = "#5a5b5c";
+    for (const prop of browserColorFallback) {
       if (!theme.colors) {
         break;
       }
       if (theme.colors[prop]) {
-        iconsColor = theme.colors[prop];
+        browserColor = theme.colors[prop];
         break;
       }
     }
@@ -49,7 +43,7 @@ class TabCenterBackground {
     const sidebarTextColor =
       theme.colors && theme.colors["sidebar_text"] ? theme.colors["sidebar_text"] : "#5a5b5c";
 
-    setButtonsActionColor(iconsColor, sidebarTextColor, windowId);
+    setButtonsActionColor(browserColor, sidebarTextColor);
   }
 
   onConnect(port) {
@@ -93,26 +87,16 @@ class TabCenterBackground {
 new TabCenterBackground();
 
 // Toolbar icon takes a different color than the sidebar header icon
-function setButtonsActionColor(browserColor, sidebarColor, windowId) {
+function setButtonsActionColor(browserColor, sidebarColor) {
   // src/tabcenter.svg but a little reduced
   const svgStr =
     "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3Csvg xmlns='http://www.w3.org/2000/svg' height='128' width='128' viewBox='0 0 16 16'%3E%3Cdefs%3E%3Csymbol id='shape'%3E%3Cpath d='M3,1h10a3,3,0,0,1,3,3v8a3,3,0,0,1,-3,3h-10a3,3,0,0,1,-3,-3v-8a3,3,0,0,1,3,-3Z M3,3h 4a1,1,0,0,1,1,1v8a1,1,0,0,1,-1,1h -4a1,1,0,0,1,-1,-1v-8a1,1,0,0,1,1,-1Z' fill-rule='evenodd' /%3E%3Ccircle cx='3.5' cy='4.5' r='.6' /%3E%3Ccircle cx='3.5' cy='6.5' r='.6' /%3E%3Ccircle cx='3.5' cy='8.5' r='.6' /%3E%3Crect x='4.75' y='4' height='1' width='2.25' rx='.5' ry='.5' /%3E%3Crect x='4.75' y='6' height='1' width='2.25' rx='.5' ry='.5' /%3E%3Crect x='4.75' y='8' height='1' width='2.25' rx='.5' ry='.5' /%3E%3C/symbol%3E%3C/defs%3E%3Cuse class='theme' id='default' fill='$fillcolor' href='%23shape' /%3E%3C/svg%3E%0A";
 
-  const browserSvgStr = svgStr.replace("$fillcolor", browserColor.replace("#", "%23"));
   browser.browserAction.setIcon({
-    path: {
-      16: browserSvgStr,
-      32: browserSvgStr,
-    },
-    windowId,
+    path: svgStr.replace("$fillcolor", browserColor.replace("#", "%23")),
   });
 
-  const sidebarSvgStr = svgStr.replace("$fillcolor", sidebarColor.replace("#", "%23"));
   browser.sidebarAction.setIcon({
-    path: {
-      16: sidebarSvgStr,
-      32: sidebarSvgStr,
-    },
-    windowId,
+    path: svgStr.replace("$fillcolor", sidebarColor.replace("#", "%23")),
   });
 }
