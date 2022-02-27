78d06c04e41ec29170f3f0ac633853abe17b9583
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-02-27 19:53:52 +0900

    Remove links.

--- src/options/options.js
+++ src/options/options.js
@@ -27,8 +27,6 @@ class Options {
       "optionsNotifyClosingManyTabs",
       "optionsAdvancedTitle",
       "optionsCustomCSS",
-      "optionsCustomCSSWikiLink",
-      "optionsCustomCSSChangelogLink",
       "optionsSaveCustomCSS",
     ];
     const changes = [];
